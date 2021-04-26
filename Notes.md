8482ae9e4b7d54433d16d0a27d20861ea3408205
GoodGuyGregory <greg.witt625@gmail.com>
2021-04-26 14:11:43 -0400

    adds test data and datetime file manipulations

---
+++ Notes.md
@@ -0,0 +1,6 @@
+## Sources
+[Removing Characters from a String (Used Method 5)](https://www.pythonpool.com/remove-character-from-string-python/#5_Remove_characters_from_string_Using_join)
+
+[Getting Elements from a DateTime Object in Python](https://www.tutorialsrack.com/articles/205/how-to-get-the-year-month-and-day-from-a-datetime-in-python)
+
+[Comparing Dates for files in Python](https://www.geeksforgeeks.org/comparing-dates-python/)
--- photoBatch.py
+++ photoBatch.py
@@ -3,6 +3,8 @@
 #  to be copied and added to a new folder
 
 import os
+import time
+import datetime
 import re
 import shutil
 
@@ -11,12 +13,21 @@ def selectivecopy():
     # Creates a Directory for Photo-Session
     name = input('enter a name for the session: ')
     os.mkdir(name)
+    dateOfSession = input('when was the photo session? (MM-DD-YYYY): ')
+    #  datetime object for comparison
+    listOfChars = ['-', '/']
+    dateOfSession = "".join(i for i in dateOfSession if i not in listOfChars)
+    searchRange = datetime.datetime(
+        int(dateOfSession[4:]), int(dateOfSession[:2]), int(dateOfSession[2:4]))
+    print(searchRange)
     currentPath = os.getcwd()
     FolderInPath = os.path.join(currentPath, name)
-    
+    #  TODO:
+    # GET THIS ELEMENT INTO A DATETIME OBJECT FOR COMPARISON
+    print('current time: ' + str(time.ctime(time.time())))
+
     # Moves the Folder to the desired location
-    outputLoc = input('where do you store your photos?: ')
-    
+    # outputLoc = input('where do you store your photos?: ')
 
     # Walks Directory for photos with the most recent date
     for folderName, subFolderNames, filenames in os.walk(os.getcwd()):
@@ -24,12 +35,18 @@ def selectivecopy():
         if folderName == FolderInPath:
             break
         else:
-            # determine 
+            # determine
             for filename in filenames:
                 # determine condition for file date
-                if :
-                    print("adding image file: %s" % filename)
-                    shutil.copy(filename, name)
+                fileCreationDate = str(datetime.datetime.fromtimestamp(
+                    os.stat(currentPath + '/' + filename).st_birthtime))
+                # TODO: USE THE DATETIME COMPARISON FOR THE CURRENTTIME.DAY, CURRENTTIME.MONTH
+                if datetime.time(creationTime) == searchRange:
+                    # TODO: APPEND TO THE DIRECTORY FOR ARCHIVING
+                    #     print(filename + str(os.stat(filename).st_mtime))
+                    # if :
+                    #     print("adding image file: %s" % filename)
+                    #     shutil.copy(filename, name)
             print('')
 
 
